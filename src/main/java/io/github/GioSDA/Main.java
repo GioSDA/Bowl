af971b3f2a89ebf62f19ca294c7cb10b833e5c22
gsdam <gsdam@DESKTOP-3EOJ9TS>
2020-12-28 14:26:53 -0800

    Interpreter Added

--- src/main/java/io/github/GioSDA/Main.java
+++ src/main/java/io/github/GioSDA/Main.java
@@ -3,14 +3,16 @@ package io.github.GioSDA;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+import io.github.GioSDA.interpreter.BowlInterpreter;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
 		LinkedList<String> argl = new LinkedList<String>(Arrays.asList(args));
 		
 		String code = argl.getFirst();
 		
+		BowlInterpreter BI = new BowlInterpreter(code, args);
 		
 	}
 
---
+++ src/main/java/io/github/GioSDA/interpreter/BowlInterpreter.java
@@ -0,0 +1,18 @@
+package io.github.GioSDA.interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BowlInterpreter {
+
+	private List<Object> list = new ArrayList<Object>();
+	private String code;
+	
+	public BowlInterpreter(String code, String[] input) {
+		this.code = code;
+		for (String i : input) {
+			list.add(input);
+		}
+	}
+	
+}
