deccddde03a842d1237f0ad28de5ea93d10b05cc
gsdam <gsdam@DESKTOP-3EOJ9TS>
2020-12-28 10:10:32 -0800

    Initial Commit

---
+++ src/main/java/io/github/GioSDA/Main.java
@@ -0,0 +1,17 @@
+package io.github.GioSDA;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> argl = new LinkedList<String>(Arrays.asList(args));
+		
+		String code = argl.getFirst();
+		
+		
+	}
+
+}
